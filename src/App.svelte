ac6e8ced3aa5fd26e02190acd8fe2a4b3c0931c9
Saqib Islam <saaqi.grw@gmail.com>
2024-11-15 00:08:45 +0500

    Add logo links and enhance logo display in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,45 @@
 <script>
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
+  import saaqiLogo from "./assets/logo.svg";
   import Counter from "./lib/Counter.svelte";
+
+  const logoLinks = [
+    {
+      link: "https://vite.dev",
+      img: viteLogo,
+      alt: "Vite Logo",
+      className: "vite",
+    },
+    {
+      link: "https://svelte.dev",
+      img: svelteLogo,
+      alt: "svelte Logo",
+      className: "svelte",
+    },
+    {
+      link: "https://saqibtech.com",
+      img: saaqiLogo,
+      alt: "Saqib Islam - UI/UX Desginer & Full Stack Developer",
+      className: "saaqi",
+    },
+  ].map((item, index) => {
+    return {
+      ...item,
+      id: `logos-${index}`,
+    };
+  });
 </script>
 
 <main>
   <div class="container vh-100 d-flex align-items-center">
     <div class="row">
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-          <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-        </a>
+        {#each logoLinks as { link, img, alt, className, id }, index (id)}
+          <a href={link} target="_blank" rel="noreferrer">
+            <img src={img} class="logo {className}" alt={alt} />
+          </a>
+        {/each}
       </div>
 
       <div class="col-12 mb-3">
@@ -36,12 +62,16 @@
         Click on the Vite and Svelte logos to learn more
       </p>
 
-      <p>Written by 
-        <a class="text-decoration-underline" href="https://saqibtech.com" title="Saqib Islam - UI/UX Desginer &amp; Full Stack Developer">
+      <p>
+        Written by
+        <a
+          class="text-decoration-underline"
+          href="https://saqibtech.com"
+          title="Saqib Islam - UI/UX Desginer &amp; Full Stack Developer"
+        >
           Saaqi
         </a>
       </p>
-
     </div>
   </div>
 </main>
@@ -58,6 +88,9 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .logo.saaqi:hover {
+    filter: drop-shadow(0 0 2em #374162);
   }
   .read-the-docs {
     color: #888;
